ad12e301d03f83e24751f009418fa31cc3f9e657
maksigit <maksi123p@gmail.com>
2018-05-07 18:25:15 +0300

    done remove task

--- todo.js
+++ todo.js
@@ -1,17 +1,43 @@
 console.log("///////////////////////////////////////// TODO LIST ///////////////////////////////////////////////////////");
 
-// let button = document.getElementsByClassName('send-task');
+let button = document.querySelector('.send-task');
+let listOutput = document.querySelector('.list-output');
 
-// console.log(button);
 
-sendTask.addEventListener('click', function (e) {
+button.addEventListener('click', function (e) {
     e.preventDefault();
     let getInput = document.getElementsByClassName('input-task')[0];
     let getInputValue = getInput.value;
-    console.log(getInputValue);
+
+    let text = document.createTextNode(getInputValue);
+
 
     let newLi = document.createElement('li');
-    newLi.innerHTML = getInputValue;
-    tasks.appendChild(newLi);
+    newLi.className = "item";
+    newLi.addEventListener('click', function (event) {
+        // console.log(event);
+        if (this.className === "line-through") {
+            this.className = ""
+        } else {
+            this.className = "line-through";
+        }
+    });
+
+    let close = document.createElement('span');
+    close.className = "close";
+    close.addEventListener('click', function (event) {
+        this.parentNode.remove();
+    });
+
+
+    let newInput = document.createElement('input');
+    newInput.setAttribute('type', 'checkbox');
+    newLi.appendChild(newInput);
+    newLi.insertBefore(close, newInput);
+    newLi.insertBefore(text, newLi.nextSibling);
+
+    console.log(newLi);
+    listOutput.appendChild(newLi);
+
 });
 
