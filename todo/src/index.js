8cccff64c6589a9ac705ce1704444c536a4bcf0b
maksigit <maksi123p@gmail.com>
2018-05-24 14:46:04 +0300

    Done

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import Input from './components/Input';
 
 ReactDOM.render(<Input />, document.getElementById('root'));
